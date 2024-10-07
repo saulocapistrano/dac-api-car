7e137c956d9474c72f3adbcff9bb22adac3a57a8
saulocapistrano <saulocapistrano@gmail.com>
2024-10-06 21:46:29 -0300

    Init commit

---
+++ src/test/java/com/ads/dacapicar/DacApicarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ads.dacapicar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DacApicarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
