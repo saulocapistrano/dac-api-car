77fc940c43c3f64b70d0c3cc83f5ac1350572b25
saulocapistrano <saulocapistrano@gmail.com>
2024-10-11 14:06:17 -0300

    feat: create exception to Car

---
+++ src/main/java/com/ads/dacapicar/exception/CarExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.ads.dacapicar.exception;
+
+import org.hibernate.tool.schema.spi.CommandAcceptanceException;
+import org.hibernate.tool.schema.spi.ExceptionHandler;
+
+public class CarExceptionHandler implements ExceptionHandler {
+
+    @Override
+    public void handleException(CommandAcceptanceException e) {
+
+    }
+}
