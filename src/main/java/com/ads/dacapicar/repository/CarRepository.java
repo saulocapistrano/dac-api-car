95886eb0c127df7eb8f1b8542e2c3ffa4fb209b0
saulocapistrano <saulocapistrano@gmail.com>
2024-10-11 14:05:43 -0300

    feat: create interface CarRepository

--- src/main/java/com/ads/dacapicar/repository/CarRepository.java
+++ src/main/java/com/ads/dacapicar/repository/CarRepository.java
@@ -2,6 +2,8 @@ package com.ads.dacapicar.repository;
 
 import com.ads.dacapicar.entities.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
 }
