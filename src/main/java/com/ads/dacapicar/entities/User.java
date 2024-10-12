d593245f2c249972854f9afb6434e83ea171d707
saulocapistrano <saulocapistrano@gmail.com>
2024-10-12 15:43:36 -0300

    feat: implement entity User

---
+++ src/main/java/com/ads/dacapicar/entities/User.java
@@ -0,0 +1,131 @@
+package com.ads.dacapicar.entities;
+
+import com.ads.dacapicar.entities.enums.TypeUser;
+import jakarta.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "user_person")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome de usuário é obrigatório")
+    private String name;
+
+    @Email(message = "O e-mail deve ser válido")
+    @NotBlank(message = "O e-mail é obrigatório")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 8, message = "A senha deve ter pelo menos 8 caracteres")
+    private String password;
+
+    @Pattern(regexp = "\\(\\d{2}\\)\\d{4,5}-\\d{4}", message = "Número de telefone inválido")
+    private String phoneNumber;
+
+    private LocalDate birthDate;
+
+    @Pattern(regexp = "\\d{11}", message = "CPF deve conter 11 dígitos")
+    private String cpf;
+
+    private LocalDate createdDate;
+
+    @Enumerated(EnumType.STRING)
+    private TypeUser typeUser;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password, String phoneNumber, LocalDate birthDate, String cpf, LocalDate createdDate, TypeUser typeUser) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+        this.cpf = cpf;
+        this.createdDate = createdDate;
+        this.typeUser = typeUser;
+    }
+
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public TypeUser getTypeUser() {
+        return typeUser;
+    }
+
+    public void setTypeUser(TypeUser typeUser) {
+        this.typeUser = typeUser;
+    }
+}
