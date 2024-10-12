1745b43af30934872848ca9d6dfab7773e851b44
saulocapistrano <saulocapistrano@gmail.com>
2024-10-12 15:46:46 -0300

    feat: create controller to acess endpoint by user

---
+++ src/main/java/com/ads/dacapicar/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.ads.dacapicar.controller;
+
+import com.ads.dacapicar.entities.dto.request.UserRequestDTO;
+import com.ads.dacapicar.entities.dto.response.UserResponseDTO;
+import com.ads.dacapicar.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> findAll() {
+        List<UserResponseDTO> users = userService.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> findById(@PathVariable Long id) {
+        UserResponseDTO user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO userRequestDTO) {
+        UserResponseDTO newUser = userService.createUser(userRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    @PostMapping("/bulk")
+    public ResponseEntity<List<UserResponseDTO>> createMultipleUsers(@RequestBody List<UserRequestDTO> userRequestDTOList) {
+        List<UserResponseDTO> newUsers = userService.createMultipleUsers(userRequestDTOList);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUsers);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> updateUser(@PathVariable Long id, @RequestBody UserRequestDTO userRequestDTO) {
+        UserResponseDTO updatedUser = userService.updateUser(id, userRequestDTO);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
