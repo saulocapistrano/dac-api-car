208673a1a35aad4c349cc2fdcf98c63e5af5943b
saulocapistrano <saulocapistrano@gmail.com>
2024-10-12 15:46:27 -0300

    feat: configuration bean on to flyway by repair migrations

---
+++ src/main/java/com/ads/dacapicar/config/FlywayConfig.java
@@ -0,0 +1,18 @@
+package com.ads.dacapicar.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean
+    public FlywayMigrationStrategy repairFlyway() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
+}
+
