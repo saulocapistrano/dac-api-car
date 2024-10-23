7c8282dd05f60bcd188a18a5715846a445457021
saulocapistrano <saulocapistrano@gmail.com>
2024-10-23 18:43:02 -0300

    fix: created password change endpoint

--- src/main/java/com/ads/dacapicar/controller/UserController.java
+++ src/main/java/com/ads/dacapicar/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.ads.dacapicar.controller;
 
+import com.ads.dacapicar.entities.User;
+import com.ads.dacapicar.entities.dto.request.PasswordChangeRequestDTO;
 import com.ads.dacapicar.entities.dto.request.UserRequestDTO;
 import com.ads.dacapicar.entities.dto.response.UserResponseDTO;
+import com.ads.dacapicar.exception.UserNotFoundException;
+import com.ads.dacapicar.repository.UserRepository;
 import com.ads.dacapicar.service.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -18,6 +23,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
     public ResponseEntity<List<UserResponseDTO>> findAll() {
@@ -47,6 +55,17 @@ public class UserController {
     public ResponseEntity<UserResponseDTO> updateUser(@PathVariable Long id, @RequestBody UserRequestDTO userRequestDTO) {
         UserResponseDTO updatedUser = userService.updateUser(id, userRequestDTO);
         return ResponseEntity.ok(updatedUser);
+    }
+
+    @PutMapping("/change-password")
+    public ResponseEntity<?> changePassword(@RequestBody PasswordChangeRequestDTO passwordChangeRequestDTO, Principal principal) {
+        String username = principal.getName();
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UserNotFoundException("Usuário não encontrado"));
+
+        userService.changePassword(user.getId(), passwordChangeRequestDTO);
+
+        return ResponseEntity.ok("Senha alterada com sucesso!");
     }
 
     @DeleteMapping("/{id}")
