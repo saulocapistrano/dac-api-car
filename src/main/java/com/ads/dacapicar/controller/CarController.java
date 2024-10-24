7161901dc0192c582058c827191d549a685b1e65
saulocapistrano <saulocapistrano@gmail.com>
2024-10-23 22:12:47 -0300

    feat: add methjot to create report by car id

--- src/main/java/com/ads/dacapicar/controller/CarController.java
+++ src/main/java/com/ads/dacapicar/controller/CarController.java
@@ -3,6 +3,7 @@ package com.ads.dacapicar.controller;
 import com.ads.dacapicar.entities.dto.request.CarRequestDTO;
 import com.ads.dacapicar.entities.dto.response.CarResponseDTO;
 import com.ads.dacapicar.service.CarService;
+import com.ads.dacapicar.service.reports.ReportService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/cars")
@@ -18,6 +20,10 @@ public class CarController {
 
     @Autowired
     private CarService carService;
+
+    @Autowired
+    private ReportService reportService;
+
 
     @GetMapping
     public ResponseEntity<List<CarResponseDTO>> findAll(){
@@ -36,12 +42,12 @@ public class CarController {
         CarResponseDTO createdCar = carService.createCar(carRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCar);
     }
+
     @PostMapping("/bulk")
     public ResponseEntity<List<CarResponseDTO>> createMultipleCars(@RequestBody List<CarRequestDTO> carRequestDTOList) {
         List<CarResponseDTO> createdCars = carService.createMultipleCars(carRequestDTOList);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCars);
     }
-
 
     @PutMapping("/{id}")
     public ResponseEntity<CarResponseDTO> updateCar(@PathVariable Long id, @RequestBody CarRequestDTO carRequestDTO) {
@@ -53,5 +59,19 @@ public class CarController {
     public ResponseEntity<Void> deleteCar(@PathVariable Long id) {
         carService.deleteCar(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/report")
+    public ResponseEntity<String> requestCarReport() throws Exception {
+        String filePath = "relatorio_carros.pdf";
+        CompletableFuture<String> reportFuture = reportService.generateCarReportAsync(filePath);
+        return ResponseEntity.ok("Relatório em geração. Verifique o status em alguns momentos.");
+    }
+
+    @GetMapping("/report/{id}")
+    public ResponseEntity<String> requestSingleCarReport(@PathVariable Long id) throws Exception {
+        String filePath = "relatorio_carro_" + id + ".pdf";
+        reportService.generateSingleCarReportPdf(id, filePath);
+        return ResponseEntity.ok("Relatório do carro com ID " + id + " em geração.");
     }
 }
